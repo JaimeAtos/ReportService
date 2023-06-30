9a5471b3db6b565cc568a422ab5c9f3fd7887533
miguel <miguel-leonardo.rodriguez@atos.net>
2023-06-30 12:17:25 -0600

    Commands Creation Report

---
+++ src/Core/Application/Features/ReportData/Commands/CreateReportDataCommand/CreateReportDataCommand.cs
@@ -0,0 +1,38 @@
+﻿using Application.Wrappers;
+using AutoMapper;
+using Domain.Repositories;
+using MediatR;
+
+namespace Application.Features.ReportData.Commands.CreateReportDataCommand;
+
+public class CreateReportDataCommand : IRequest<Response<Guid>>
+{
+    public string ReportKey { get; set; } = null!;
+    public string Data { get; set; } = null!;
+}
+
+public class CreateReportDataCommandHandler : IRequestHandler<CreateReportDataCommand, Response<Guid>>
+{
+    private readonly IReportBaseRepository _repository;
+    private readonly IMapper _mapper;
+
+    public CreateReportDataCommandHandler(IReportBaseRepository repository, IMapper mapper)
+    {
+        _repository = repository;
+        _mapper = mapper;
+    }
+
+    public Task<Response<Guid>> Handle(CreateReportDataCommand request, CancellationToken cancellationToken)
+    {
+        if (request is null)
+            throw new ArgumentNullException(nameof(request), "Request must not be empty");
+        return ProcessHandle(request, cancellationToken);
+    }
+
+    private async Task<Response<Guid>>  ProcessHandle(CreateReportDataCommand request, CancellationToken cancellationToken)
+    {
+        var report = _mapper.Map<Domain.Entities.ReportBase>(request);
+        var record = await _repository.AddAsync(report, cancellationToken);
+        return new Response<Guid>(record.Id);
+    }
+}
